f2e06d365b708f91851b926bddebaa8817a8d677
jorneylm <jorney95@live.com>
2021-08-05 23:53:33 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
+import React from 'react'
+import styled from '@emotion/styled';
+// import  from './components/Frase';
 
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+` ;
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family:  Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  transition: background-size .8s ease;
+  :hover {
+    cursor:pointer;
+    background-size: 400px;
+  }
+`;
+
+const consultarAPI = () => {
+  console.log("Consultar...")
+}
 
 function App() {
   return (
-   <h2>Breaking Bad</h2>
+    <Contenedor>
+      <Boton
+        onClick={consultarAPI}
+      >
+        Obtener Frase
+      </Boton>
+    </Contenedor>
   );
 }
 
